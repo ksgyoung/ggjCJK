493879eba35705f720d41d88139a257a1f13d379
pypmannetjies@gmail.com <pypmannetjies@gmail.com>
2014-01-24 22:18:43 +0200

    Controllers

--- src/controllers/WifiController.java
+++ src/controllers/WifiController.java
@@ -2,5 +2,16 @@ package controllers;
 
 public class WifiController {
 	
+	public double getConnectionStrength() {
+		return 0;
+	}
+	
+	public boolean isInternetConnected() {
+		return false;
+	}
+	
+	public boolean isFirstConnect() {
+		return false;
+	}
 
 }
